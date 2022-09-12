6ac1756828d2095840d6c40b2af6214717b9119b
Osemenam Desmond <osemenamudoka429@gmail.com>
2022-09-12 06:26:42 -0700

    puttings

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 
 /**
- * main - entry point
- *
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * return: 0 
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
