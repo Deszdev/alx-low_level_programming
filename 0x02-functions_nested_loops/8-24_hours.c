dc7c820dade5e664693e8383eb0026406d175b56
Osemenam Desmond <osemenamudoka429@gmail.com>
2022-09-13 07:45:08 -0700

    betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,10 +7,10 @@
 */
 void jack_bauer(void)
 {
-	int i,j
+	int i, j;
 
 	i = 0;
-	while (i < 24);
+	while (i < 24)
 	{
 		j = 0;
 		while (j < 60)
@@ -21,8 +21,8 @@ void jack_bauer(void)
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
-			j++
+			j++;
 		}
-		i++
+		i++;
 	} 
 }
