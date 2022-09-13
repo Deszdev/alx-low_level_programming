caeb6390bca03c3bcfd5cc953dbba11cc8ceec00
Osemenam Desmond <osemenamudoka429@gmail.com>
2022-09-13 06:56:36 -0700

    butty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *print_last_digit a function that prints last digit
- * @x: x is the function parameter   
- * Return: int
+ *print_last_digit - a function that prints last digit
+ *@x: x is the function parameter   
+ *Return: int
  */
 int print_last_digit(int x)
 {
