717471069d49cf0a0b8f2fcb38d32a2562feac67
Osemenam Desmond <osemenamudoka429@gmail.com>
2022-09-14 06:23:38 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-*print_last_digit - a function that prints last digit
-*@x: x is the function parameter   
-*Return: int
-*/
+ * print_last_digit a function that prints a last digit
+ * @x: x is the function parameter
+ *
+ * Return: int
+ */
 int print_last_digit(int x)
 {
 	int lastno = x % 10;
