df2ca585c17747bd978da7b16c4c2dca50648a42
Osemenam Desmond <osemenamudoka429@gmail.com>
2022-09-14 06:12:30 -0700

    add2

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* add - add is a function that adds two numbers
-* @a: the first parameter
-* @b: the second parameter
-*       
-* Return: int
-*/
+ * add - add is a function that adds two numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ *
+ * Return: int
+ */
 int add(int a, int b)
 {
 	return (a + b);
